43f0e3f186d6a2425312df535d11bf5ef587581f
newuser <newuser@newusers-iMac.home>
2015-06-14 19:52:35 -0400

    Initial Commit

---
+++ AngryMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AngryMe
+//
+//  Created by newuser on 6/14/15.
+//  Copyright (c) 2015 Power Streamer LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
